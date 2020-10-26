8ee8d830a613676ddbf693351d76053e69b3e3cb
faultyagatha <valeria.barvinska@gmail.com>
2020-10-26 15:52:03 +0100

    add error handling middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,8 +1,22 @@
 import express from 'express';
 import { json } from 'body-parser';
 
+import { router as userRouter } from './routes/user';
+import { router as signinRouter } from './routes/signin';
+import { router as signupRouter } from './routes/signup';
+import { router as signoutRouter } from './routes/signout';
+import { errorHandler } from './middlewares/errorHandler';
+
 const app = express();
 app.use(json());
+
+//routes
+app.use(userRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.use(errorHandler);
 
 const port = 3000;
 app.listen(port, () => {
---
+++ auth/src/routes/user.ts
@@ -0,0 +1,13 @@
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
+
+const router = express.Router();
+
+router.get('api/users/user',
+  (req: Request, res: Response) => {
+    console.log('getting a user');
+    res.send({});
+
+  });
+
+export { router };
